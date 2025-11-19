faa3bcb95d4b98cee01f6a78624c555c1a24da6e
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2025-11-19 20:49:50 +0800

    INITAL COMMIT

---
+++ bot.py
@@ -0,0 +1,21 @@
+import ssl, certifi
+ssl._create_default_https_context = lambda *args, **kwargs: ssl.create_default_context(cafile=certifi.where())
+import flask
+import slack
+import os
+import pathlib
+from pathlib import Path
+from dotenv import load_dotenv
+from slackeventsapi import SlackEventAdapter
+
+
+env_path = Path('..') / '.env'
+load_dotenv(dotenv_path=env_path)
+app = flask.Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.getenv("SIGNING_TOKEN"), '/slack/events', app)
+client = slack.WebClient(token=os.getenv("SLACK_TOKEN"))
+
+client.chat_postMessage(channel="#a-space-mans-journey", text="Good morning yall!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
